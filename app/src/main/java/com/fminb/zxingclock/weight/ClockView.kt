ee0f256bf9cdff09618bd9f381d27ce699d9cbc0
ZhxMiao <mzx1217@gmail.com>
2024-12-19 20:29:39 +0800

    feat(背景)：水平垂直线

--- app/src/main/java/com/fminb/zxingclock/weight/ClockView.kt
+++ app/src/main/java/com/fminb/zxingclock/weight/ClockView.kt
@@ -7,6 +7,7 @@ import android.icu.util.Calendar
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.animation.doOnStart
+import com.fminb.zxingclock.R
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -23,11 +24,17 @@ class ClockView @JvmOverloads constructor(
         strokeCap = Paint.Cap.ROUND
     }
 
-    private val textPaint = Paint().apply {
+    private val backgroundBitmap: Bitmap = BitmapFactory.decodeResource(resources, R.mipmap.bg)
+    private lateinit var scaledBackgroundBitmap: Bitmap
+
+    private var length: Float = 240f
+
+    private val crossHairPaint = Paint().apply {
         isAntiAlias = true
-        textAlign = Paint.Align.CENTER
-        textSize = 30f
-        color = Color.BLACK
+        style = Paint.Style.STROKE
+        strokeWidth = 3f
+        color = Color.parseColor("#603ED7")
+        pathEffect = DashPathEffect(floatArrayOf(10f, 10f), 0f)
     }
 
     private val centerPointPaint = Paint().apply {
@@ -66,6 +73,17 @@ class ClockView @JvmOverloads constructor(
         val height = MeasureSpec.getSize(heightMeasureSpec)
         val size = minOf(width, height)
         setMeasuredDimension(size, size)
+        length = size * 0.2f
+        val newSize = (size * 0.93).toInt()
+        // 如果还没有缩放背景图片，则进行缩放
+        if (!::scaledBackgroundBitmap.isInitialized || scaledBackgroundBitmap.width != newSize) {
+            try {
+                scaledBackgroundBitmap =
+                    Bitmap.createScaledBitmap(backgroundBitmap, newSize, newSize, false)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -76,11 +94,36 @@ class ClockView @JvmOverloads constructor(
         val radius = minOf(centerX, centerY) * 0.75f
 
         drawClockFace(canvas, centerX, centerY, radius)
+        drawCrossHair(canvas, centerX, centerY)
         drawHands(canvas, centerX, centerY, radius)
         drawCenterPoint(canvas, centerX, centerY)
     }
 
+    private fun drawCrossHair(canvas: Canvas, centerX: Float, centerY: Float) {
+        // 绘制垂直线
+        canvas.drawLine(
+            centerX,
+            centerY - length / 2,
+            centerX,
+            centerY + length / 2,
+            crossHairPaint
+        )
+
+        // 绘制水平线
+        canvas.drawLine(
+            centerX - length / 2,
+            centerY,
+            centerX + length / 2,
+            centerY,
+            crossHairPaint
+        )
+    }
+
     private fun drawClockFace(canvas: Canvas, centerX: Float, centerY: Float, radius: Float) {
+        // 绘制背景图片
+        val bitmapLeft = centerX - scaledBackgroundBitmap.width / 2
+        val bitmapTop = centerY - scaledBackgroundBitmap.height / 2
+        canvas.drawBitmap(scaledBackgroundBitmap, bitmapLeft, bitmapTop, paint)
         paint.color = Color.parseColor("#5C4FCC")
         canvas.drawCircle(centerX, centerY, radius, paint)
     }
@@ -93,22 +136,22 @@ class ClockView @JvmOverloads constructor(
         val second = calendar.get(Calendar.SECOND)
         val millisecond = calendar.get(Calendar.MILLISECOND)
 
-        // Calculate the exact position of the second hand
+        // Calculate the exact position of the second
         val secondAngle = (second * 6 + millisecond / 1000.0 * 6) % 360
 
-        // Draw the hour hand
+        // Draw the hour
         paint.strokeWidth = 15f
         paint.color = Color.parseColor("#8670D4")
         val hourAngle = ((hour * 30 + minute / 60.0 * 30) % 360).toFloat()
         drawHand(canvas, centerX, centerY, radius * 0.43f, hourAngle)
 
-        // Draw the minute hand
+        // Draw the minute
         paint.strokeWidth = 12f
         paint.color = Color.parseColor("#8670D4")
         val minuteAngle = ((minute * 6 + second / 60.0 * 6) % 360).toFloat()
         drawHand(canvas, centerX, centerY, radius * 0.72f, minuteAngle)
 
-        // Draw the second hand
+        // Draw the second
         paint.strokeWidth = 3f
         paint.color = Color.parseColor("#CBBDFD")
         drawHand(canvas, centerX, centerY, radius * 0.85f, secondAngle.toFloat())
