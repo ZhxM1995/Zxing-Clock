7a389304cfb61ac559cde13b24fbbe8e2aae9692
ZhxMiao <mzx1217@gmail.com>
2024-12-20 16:55:32 +0800

    feat(改标题栏颜色)

--- app/src/main/java/com/fminb/zxingclock/MainActivity.kt
+++ app/src/main/java/com/fminb/zxingclock/MainActivity.kt
@@ -2,10 +2,14 @@ package com.fminb.zxingclock
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.fminb.zxingclock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
